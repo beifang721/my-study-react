c16aca85edf22e57984a941fa3545e000c81fbe6
yangqianjin <763423772@qq.com>
2019-05-14 21:50:31 +0800

    #  我就想传

--- demo01/src/antd/TodoList.js
+++ demo01/src/antd/TodoList.js
@@ -4,32 +4,46 @@ import { Input, Button, List } from 'antd';
 import store from '../store';
 
 export default class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = store.getState();
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
+  render() {
+    return (
+      <div style={{ marginTop: '10px', marginLeft: '10px' }}>
+        <div>
+          <Input
+            value={this.state.inputValue}
+            placeholder="todo info"
+            style={{ width: '300px', marginRight: '10px' }}
+            onChange={this.handleChangeInput}
+          />
+          <Button type="primary">提交</Button>
+        </div>
+        <List
+          size="small"
+          style={{ width: '400px', marginTop: '10px' }}
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={this.state.list}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+      </div>
+    )
+  }
+
+  handleChangeInput(e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
     }
-    render() {
-        return (
-            <div style={{ marginTop: '10px', marginLeft: '10px' }}>
-                <div>
-                    <Input 
-                        value={this.state.inputValue} 
-                        placeholder="todo info" 
-                        style={{ width: '300px', marginRight: '10px' }} 
-                        
-                        />
-                    <Button type="primary">提交</Button>
-                </div>
-                <List
-                    size="small"
-                    style={{width: '400px',marginTop: '10px'}}
-                    header={<div>Header</div>}
-                    footer={<div>Footer</div>}
-                    bordered
-                    dataSource={this.state.list}
-                    renderItem={item => <List.Item>{item}</List.Item>}
-                    />
-            </div>
-        )
-    }
+    store.dispatch(action);
+  }
+  handleStoreChange() {
+    this.setState(store.getState())
+  }
 }
--- demo01/src/store/reducer.js
+++ demo01/src/store/reducer.js
@@ -1,7 +1,13 @@
 const defaultState = {
-    inputValue: 'xx',
+    inputValue: '123',
     list: ['aa']
 };
 export default (state = defaultState, action) => {
+    if(action.type === "change_input_value"){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    console.log(state,action);
     return state;
 }
